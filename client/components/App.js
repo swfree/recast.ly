66401eb9dbd46e4000fe398a23b5e5eea864b414
swfree <skye.free@gmail.com>
2016-03-18 16:23:48 -0700

    Create dynamic video player

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,31 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentVideoList: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  videoListEntryOnClick() {
+    this.setState({
+      currentVideo: () => console.log('clicked')
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.currentVideoList} state={this.state}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(< App />, document.getElementById('app'));
--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -1,13 +1,17 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="//i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+var VideoListEntry = (props) => {
+  varchangeCurrentVideo 
+
+  return (
+    <div className="video-list-entry">
+      <div className="media-left media-middle">
+        <img className="media-object" src={props.data.snippet.thumbnails.default.url} alt="" />
+      </div>
+      <div className="media-body">
+        <div className="video-list-entry-title">{props.data.snippet.title}</div>
+        <div className="video-list-entry-detail">{props.data.snippet.description}</div>
+      </div>
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+  );
+}
 
 window.VideoListEntry = VideoListEntry;
